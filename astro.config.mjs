2e7294b797b4b8260c501b6719705c5f0b584091
Raul <rulanxa@gmail.com>
2024-12-02 17:32:08 +0100

    base url changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://d0vak1n.github.com/',
+  site: 'https://d0vak1n.github.io/ramoweb/',
   base: '/',
 });
