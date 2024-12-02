8d5b8d933899cc051edf6df71b8f4ef7cf0ecb2c
Raul <rulanxa@gmail.com>
2024-12-02 17:26:20 +0100

    feat: base url added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://d0vak1n.github.com/',
+  base: '/',
 });
