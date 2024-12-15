bbc4e74516d5c82764fb98577eff21ffb959310f
Raul <rulanxa@gmail.com>
2024-12-15 23:34:12 +0100

    feat: add support robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,38 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
+import robots from 'astro-robots';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ramoweb.com',
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robots({
+      host: "ramoweb.com",
+      sitemap: [
+        "https://ramoweb.com/sitemap-0.xml",
+        "https://www.ramoweb.com/sitemap-0.xml",
+      ],
+      policy: [
+        {
+          userAgent: [
+            "Applebot",
+            "Googlebot",
+            "bingbot",
+            "Yandex",
+            "Yeti",
+            "Baiduspider",
+            "360Spider",
+            "*",
+          ],
+          allow: ["/"],
+          disallow: ["/_astro"],
+          crawlDelay: 5,
+        },
+      ],
+    }),
+  ],
 });
