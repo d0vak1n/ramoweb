6a1f9a273c94939b8c46ee15ce66a88f7be83e0b
Raul <rulanxa@gmail.com>
2025-02-24 21:42:19 +0100

    fix: fixes problems upgrading to tailwindcss 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import robots from 'astro-robots';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [
+      tailwindcss()
+    ],
+  },
   integrations: [
-    tailwind(),
     sitemap(),
     robots({
       host: "ramoweb.com",
